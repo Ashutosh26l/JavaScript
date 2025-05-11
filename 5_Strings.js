4d1221c13f5fd2e0f3522fb02417cb494280eb20
ashutosh26l <aksmba2611@gmail.com>
2025-05-11 09:23:13 +0530

    new String

---
+++ 5_Strings.js
@@ -0,0 +1,8 @@
+let repo=50;
+let name="ashutosh";
+
+
+console.log(`hello my name is ${name.toUpperCase()} and my repo count is ${repo}`);
+
+const gameName=new String("ashutosh");
+console.log(gameName);// String ko aise bhi declare kiya ja saakta hi it provides different usaage when it come in term of usaage in browser(see running it in console of browser)
