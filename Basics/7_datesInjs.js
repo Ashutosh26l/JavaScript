ef3acd7ddcbc6795ba55bf12c37a1f7a6457ffe7
ashutosh26l <aksmba2611@gmail.com>
2025-05-11 23:07:52 +0530

    .

---
+++ Basics/7_datesInjs.js
@@ -0,0 +1,19 @@
+//dates
+
+let myDate=new Date();
+console.log(myDate);
+console.log(myDate.getFullYear());
+console.log(myDate.getMonth());
+console.log(myDate.getDate());
+console.log(myDate.getDay());
+console.log(myDate.getHours());
+console.log(myDate.getMinutes());
+console.log(myDate.getTime());
+console.log(myDate.getTimezoneOffset());
+console.log(myDate.getMilliseconds());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toLocaleString());
+
+let myCreatedDte=new Date(2023,0,23);
+console.log(myCreatedDte.toDateString());
